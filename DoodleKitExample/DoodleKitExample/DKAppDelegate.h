ab9c43bd29f3cbf17a7a553ab7b3374a66cb27a8
Alex <alexbelliotti@gmail.com>
2013-07-12 20:39:28 -0700

    added example for great success of doodlekit

---
+++ DoodleKitExample/DoodleKitExample/DKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DKAppDelegate.h
+//  DoodleKitExample
+//
+//  Created by Alexander Belliotti on 7/12/13.
+//  Copyright (c) 2013 Alexander Belliotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DoodleKitExample/DoodleKitExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DoodleKitExample
+//
+//  Created by Alexander Belliotti on 7/12/13.
+//  Copyright (c) 2013 Alexander Belliotti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DKAppDelegate class]));
+    }
+}

4dfe8d5d48793bdc3ad58b5ce64d280757365032
Chris Hellmuth <chellmuth@crowdcompass.com>
2013-07-13 13:45:40 -0700

    Start refactoring

--- DoodleTester/GTViewController.m
+++ DoodleTester/GTViewController.m
@@ -104,24 +104,16 @@ static NSInteger const StartGameFlag = 1 << 3;
     self.match.delegate = self;
     if (match.expectedPlayerCount == 0) {
         [self dismissViewControllerAnimated:YES completion:nil];
-        [self.match chooseBestHostPlayerWithCompletionHandler:^(NSString *playerID) {
-            NSLog(@"%@", self.match);
-            NSLog(@"%@", playerID);
 
-            NSString *myIdentifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
-            [self.deviceIDs addObject:myIdentifier];
-            NSError *error;
-            [self sendHostTest:myIdentifier];
-            if (error) { assert(0); }
-        }];
+        NSString *myIdentifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+        [self.deviceIDs addObject:myIdentifier];
+        [self sendHostTest:myIdentifier];
     }
 }
 
 - (void)sendHostTest:(NSString *)deviceID {
     NSData *payload = [self payloadForDictionary:@{ @"DeviceID": deviceID } withFlag:HostTestFlag];
-    NSError *error;
-    [self.match sendDataToAllPlayers:payload withDataMode:GKMatchSendDataReliable error:&error];
-    if (error) { assert(0); }
+    [self sendDataToAllPlayers:payload];
 
 }
 - (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didFindPlayers:(NSArray *)playerIDs {
@@ -131,85 +123,6 @@ static NSInteger const StartGameFlag = 1 << 3;
 - (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didReceiveAcceptFromHostedPlayer:(NSString *)playerID {
     
 }
-
-//- (void)didTouchButton {
-//    GKMatchRequest *request = [[GKMatchRequest alloc] init];
-//    request.minPlayers = 2;
-//    request.maxPlayers = 2;
-//    request.defaultNumberOfPlayers = 2;
-//    request.playersToInvite = [self.playersToInvite allObjects];
-//    request.inviteeResponseHandler = ^(NSString *playerID, GKInviteeResponse response)
-//    {
-//        if (response == GKInviteeResponseAccepted && self.match.expectedPlayerCount == 0) {
-//            GKMatchmaker *matchMaker = [GKMatchmaker sharedMatchmaker];
-//            [matchMaker finishMatchmakingForMatch:self.match];
-//            
-//            NSError *newError;
-//            [self.match sendDataToAllPlayers:[@"HELLO WORLD" dataUsingEncoding:NSUTF8StringEncoding] withDataMode:NSUTF8StringEncoding error:&newError];
-//            if (newError) {
-//                assert(NO);
-//            }
-//        }
-//    };
-//    
-//    [[GKMatchmaker sharedMatchmaker] findMatchForRequest:request withCompletionHandler:^(GKMatch *match, NSError *error) {
-//        NSLog(@"FIND MATCH");
-//        NSLog(@"%@", match);
-//        NSLog(@"%@", match.playerIDs);
-//        NSLog(@"%@", error);
-//        if (!error) {
-//            self.match = match;
-//            self.match.delegate = self;
-//        }
-//        
-//    }];
-//}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-
-- (void)startSearching
-{
-//    __weak GKMatchmaker *matchmaker = [GKMatchmaker sharedMatchmaker];
-//    [matchmaker setInviteHandler:^(GKInvite *acceptedInvitation, NSArray *players) {
-//        assert(!players);
-//        NSLog(@"INVITE HANDLER");
-//        NSLog(@"%@", acceptedInvitation);
-//        NSLog(@"%@", players);
-//        
-//        [matchmaker matchForInvite:acceptedInvitation completionHandler:^(GKMatch *match, NSError *error) {
-//            NSLog(@"MatchForInvite");
-//            NSLog(@"%@", match);
-//            NSLog(@"%@", error);
-//            NSLog(@"%d", match.expectedPlayerCount);
-//            NSLog(@"%@", match.playerIDs);
-//            
-//            self.match = match;
-//            self.match.delegate = self;
-//
-//            NSError *newError;
-//            [self.match sendDataToAllPlayers:[@"HELLO WORLD" dataUsingEncoding:NSUTF8StringEncoding] withDataMode:NSUTF8StringEncoding error:&error];
-//            if (newError) {
-//                assert(NO);
-//            }
-//            if (match.expectedPlayerCount == 0) {
-//                [matchmaker finishMatchmakingForMatch:match];
-//            }
-//            
-//            
-//        }];
-//    }];
-//    [[GKMatchmaker sharedMatchmaker] startBrowsingForNearbyPlayersWithReachableHandler:^(NSString *playerID, BOOL reachable) {
-//        if (reachable) {
-//            [self.playersToInvite addObject:playerID];
-//        }
-//    }];
-}
-
-
-// The match received data sent from the player.c
 
 - (NSInteger)flagFromPayload:(NSData *)payload {
     NSInteger flag;
@@ -221,6 +134,20 @@ static NSInteger const StartGameFlag = 1 << 3;
     NSDictionary *dictionary = [NSKeyedUnarchiver unarchiveObjectWithData:
                              [payload subdataWithRange:NSMakeRange(sizeof(NSInteger), [payload length] - sizeof(NSInteger))]];
     return dictionary;
+}
+
+- (void)sendDataToAllPlayers:(NSData *)data {
+    NSLog(@"Sending data to all players");
+    NSError *error;
+    [self.match sendDataToAllPlayers:data withDataMode:GKMatchSendDataReliable error:&error];
+    if (error) { assert(0); }
+}
+
+- (void)sendDataToHost:(NSData *)data {
+    NSLog(@"Sending data to host");
+    NSError *error;
+    [self.match sendData:data toPlayers:@[ self.hostPlayerID ] withDataMode:GKMatchSendDataReliable error:&error];
+    if (error) { assert(0); }
 }
 
 - (void)receivedHostTestDictionary:(NSDictionary *)dictionary {
@@ -240,17 +167,13 @@ static NSInteger const StartGameFlag = 1 << 3;
 }
 
 - (void)sendHostConfirmation {
-    NSData *payload = [self payloadForDictionary:@{ @"message-id": @"you are the host" } withFlag:HostTestFlag];
-    NSError *error;
-    [self.match sendData:payload toPlayers:@[ self.hostPlayerID ] withDataMode:GKMatchSendDataReliable error:&error];
-    if (error) { assert(0); }
+    NSData *payload = [self payloadForDictionary:@{ @"message-id": @"you are the host" } withFlag:HostConfirmFlag];
+    [self sendDataToHost:payload];
 }
 
 - (void)sendStartGame {
     NSData *payload = [self payloadForDictionary:@{ @"message-id": @"start game" } withFlag:StartGameFlag];
-    NSError *error;
-    [self.match sendDataToAllPlayers:payload withDataMode:GKMatchSendDataReliable error:&error];
-    if (error) { assert(0); }
+    [self sendDataToAllPlayers:payload];
 
     [self startGame];
 }
@@ -265,7 +188,6 @@ static NSInteger const StartGameFlag = 1 << 3;
 - (void)receivedHostClaimDictionary:(NSDictionary *)dictionary fromPlayer:(NSString *)playerID {
     self.hostPlayerID = playerID;
     [self sendHostConfirmation];
-    NSLog(@"%@", dictionary);
 }
 
 - (void)startGame {
@@ -281,6 +203,7 @@ static NSInteger const StartGameFlag = 1 << 3;
     NSDictionary *dictionary = [self dictionaryFromPayload:data];
 
 
+    NSLog(@"%d, %@", flag, dictionary);
     switch (flag) {
         case HostTestFlag: {
             [self receivedHostTestDictionary:dictionary];
@@ -316,10 +239,7 @@ static NSInteger const StartGameFlag = 1 << 3;
     self.isHost = YES;
 
     NSData *payload = [self payloadForDictionary:@{ @"message-id": @"become-host" } withFlag:HostClaimFlag];
-    NSError *error;
-    [self.match sendDataToAllPlayers:payload withDataMode:GKMatchSendDataReliable error:&error];
-    if (error) { assert(0); }
-
+    [self sendDataToAllPlayers:payload];
 }
 
 // The player state changed (eg. connected or disconnected)
@@ -336,5 +256,7 @@ static NSInteger const StartGameFlag = 1 << 3;
 - (BOOL)match:(GKMatch *)match shouldReinvitePlayer:(NSString *)playerID {
     return YES;
 }
+
+- (void)startSearching {}
 
 @end

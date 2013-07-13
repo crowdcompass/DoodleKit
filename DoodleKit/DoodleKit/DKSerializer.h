4d48340525c05abbb7135161432ba1f4984e6c47
Ryan Crosby <rcrosby@crowdcompass.com>
2013-07-12 22:45:01 -0700

    Added DKSerializer interface definition

--- DoodleKit/DoodleKit/DKSerializer.h
+++ DoodleKit/DoodleKit/DKSerializer.h
@@ -8,6 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+#warning Move these to another file
+// Move these into a separate file
+enum _DKDoodleToolType {
+    DKDoodleToolPen = 0,
+};
+typedef NSUInteger DKDoodleToolType;
+
+//NSValue *anObj = [NSValue value:&struct withObjCType:@encode(aStruct)];
+struct DKPenPoint {
+    CGPoint previousPreviousPoint;
+    CGPoint previousPoint;
+    CGPoint currentPoint;
+};
+
 @interface DKSerializer : NSObject
+
+- (void)startUsingTool:(DKDoodleToolType)toolType;
+- (void)setInitialPoint:(CGPoint)point;
+- (void)addDKPointData:(NSValue *)pointData;
+- (void)finishUsingTool;
 
 @end
